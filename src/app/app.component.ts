c481c69d6227c65c72d096d1a93bde240e28066a
Øystein <oysteinha@gmail.com>
2022-05-11 15:59:46 +0200

    Obserable
    Subject, next etc..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'observables';
+  userActivated = false;
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    })
+  }
 }
